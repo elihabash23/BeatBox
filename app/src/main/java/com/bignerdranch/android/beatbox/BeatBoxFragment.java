adcf32e155a6a7f424671e219a80db5465254c4c
Elias Habash <eliashabash@aol.com>
2017-07-13 01:43:39 -0700

    Added sounds, created the Sound class

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -7,14 +7,25 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by eliballislife11 on 7/12/17.
  */
 
 public class BeatBoxFragment extends Fragment {
+
+    private BeatBox mBeatBox;
+
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBeatBox = new BeatBox(getActivity());
     }
 
     @Override
@@ -24,7 +35,37 @@ public class BeatBoxFragment extends Fragment {
 
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.fragment_beat_box_recycler_view);
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        recyclerView.setAdapter(new SoundAdapter());
 
         return view;
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder {
+        private Button mButton;
+
+        public SoundHolder(LayoutInflater inflater, ViewGroup container) {
+            super(inflater.inflate(R.layout.list_item_sound, container, false));
+
+            mButton = (Button)itemView.findViewById(R.id.list_item_sound_button);
+        }
+    }
+
+    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+
+        @Override
+        public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            return new SoundHolder(inflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(SoundHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
---
+++ app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
@@ -0,0 +1,30 @@
+package com.bignerdranch.android.beatbox;
+
+/**
+ * Created by eliballislife11 on 7/13/17.
+ *
+ * The Sound class is responsible for keeping track of the filename,
+ * the name the user should see, and any other information related
+ * to that sound.
+ */
+
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+}
